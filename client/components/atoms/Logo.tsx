bda5c329677aff6f2193c3f5b0bc9aaddaefbfd7
JustCheburek <artemiznakhdki@gmail.com>
2025-05-23 15:44:03 +1000

    footer

--- client/components/atoms/Logo.tsx
+++ client/components/atoms/Logo.tsx
@@ -8,7 +8,7 @@ interface LogoProps {
 export default function Logo({ className }: LogoProps) {
   return (
     <Link href="/" className={`flex items-center gap-2 ${className}`}>
-      <TrendingUp className="h-6 w-6 text-green-600" />
+      <TrendingUp className="h-6 w-6 text-primary" />
       <span className="font-bold text-xl">EconoNews</span>
     </Link>
   )
